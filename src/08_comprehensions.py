8515b5cc46dd82f9e6e393939119142bd465992c
Poenomaly <djbenson741@gmail.com>
2020-08-04 14:08:31 -0600

    Complete module 1 exercises

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -9,33 +9,36 @@ for more info regarding list comprehensions.
 """
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
+def comprehensions():
+    y = [i for i in range(1,6)]
 
-y = []
+    print (y)
 
-print (y)
+    # Write a list comprehension to produce the cubes of the numbers 0-9:
+    # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-# Write a list comprehension to produce the cubes of the numbers 0-9:
-# [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
+    y = [i**3 for i in range(0,10)]
 
-y = []
+    print(y)
 
-print(y)
+    # Write a list comprehension to produce the uppercase version of all the
+    # elements in array a. Hint: "foo".upper() is "FOO".
 
-# Write a list comprehension to produce the uppercase version of all the
-# elements in array a. Hint: "foo".upper() is "FOO".
+    a = ["foo", "bar", "baz"]
 
-a = ["foo", "bar", "baz"]
+    y = [val.upper() for val in a]
 
-y = []
+    print(y)
 
-print(y)
+    # Use a list comprehension to create a list containing only the _even_ elements
+    # the user entered into list x.
 
-# Use a list comprehension to create a list containing only the _even_ elements
-# the user entered into list x.
+    x = input("Enter comma-separated numbers: ").split(',')
 
-x = input("Enter comma-separated numbers: ").split(',')
+    # What do you need between the square brackets to make it work?
+    y = [i for i in x if int(i) % 2 == 0]
 
-# What do you need between the square brackets to make it work?
-y = []
+    print(y)
 
-print(y)
+if __name__ == "__main__":
+    comprehensions()
